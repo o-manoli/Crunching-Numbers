230c07ef0abf330e1516af68eb0f3186f98864ef
Marloows <131553776+o-manoli@users.noreply.github.com>
2023-10-14 03:07:29 +0200

    Sieve of Eratosthenes C-Code

---
+++ Hunting-Primes/Sieve-of-Eratosthenes/C-Code/main.c
@@ -0,0 +1,77 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void print(int *t){
+	int s = *t++;
+	while(s--)
+		printf("%d\n", *t++);
+}
+
+int *sieve(const int Up2){
+
+	if (Up2 < 2)
+		return calloc(0, sizeof(int));
+
+	const int SIZE = (Up2+1)/2;
+	int* const Re = malloc( (SIZE+1) * sizeof(int));
+	*Re = SIZE;
+
+
+	// tmp
+	int S;		int *t, *s;
+
+	// Aliases
+	int* const N = Re +1;	// #cell-shift => start at 2
+
+	// 1. Populate
+	t = N;
+	*t++ = 2;	s = t;	*t++ = 3;
+
+	S = SIZE-2;	// start loop at cell 5
+	while(S--)
+		*t++ = 2 + *s++;
+
+	// 2. Eliminate
+
+	int p = 3;	int p2;
+	t = N+1;
+
+	while(p*p <= Up2){
+
+		p2 = p<<1;		// cache 2*p
+
+		for (int i = p*p; i <= Up2 ; i += p2)
+			N[i>>1] = 0;
+
+		while (!*++t);
+		p = *t;
+	}
+
+	S = SIZE;	t = N;	s = t-1;
+
+	while(S--)
+		if (*++s)	*t++ = *s;
+
+	S = t - Re;
+	*Re = S - 1;
+
+	return realloc(Re, S * sizeof(int));
+}
+
+int main(int argc, char* argv[]){
+
+	int Up2 = (argc < 2) ? 7919 : atoi(argv[1]);	// the 1000th prime
+
+	int* const primes = sieve(Up2);
+
+	if (argc > 2){
+		if (argv[2][0] == 'w')
+			print(primes);
+	} else
+		printf("\n\n\t%d Primes found Up to %d\n\n", *primes, Up2);
+
+	free(primes);
+
+	return 0;
+}
+
---
+++ Hunting-Primes/Sieve-of-Eratosthenes/readme.md
@@ -0,0 +1,10 @@
+# [Sieve of Eratosthenes](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes) Implementation
+
+## Reference
+
+> [The PrimePages: How Many Primes are There?](https://t5k.org/howmany.html)
+
+## [C-Code](./C-Code/main.c)
+
+  Worked on my machine :)
+
